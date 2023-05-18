03af9767712047e5c33298a1c2bdda027cd68d17
hoangthanhlamm <hoangthanhlamm@gmail.com>
2023-05-18 12:09:14 +0700

    fix: setup

--- setup.py
+++ setup.py
@@ -26,7 +26,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=[
-        "pytest==7.3.1"
-    ],
+    install_requires=[],
 )
